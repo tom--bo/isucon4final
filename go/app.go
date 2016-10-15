01333f8695536c30bfc41f9e7765304e2b3ff0d5
Kender09 <k.ikeuchi@dcl.cs.waseda.ac.jp>
2016-10-15 07:42:12 +0000

    Fix post webdav protocol

--- go/app.go
+++ go/app.go
@@ -216,7 +216,7 @@ func getLog(id string) map[string][]ClickLog {
 }
 
 func postWebdav(ipaddr string, buf io.Reader) {
-	req, err := http.NewRequest("POST", ipaddr, buf)
+	req, err := http.NewRequest("PUT", ipaddr, buf)
 	if err != nil {
 		fmt.Println(err)
 		return
